d75d4c461c9a5fb23c9a3bb2465fabc5e2d7c414
Vignesh Aalathur <85474399+vignesha04@users.noreply.github.com>
2024-04-06 16:49:02 +0530

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,7 +6,7 @@ export default class Nav extends Component {
     return (
       <nav>
           <div className="nav-wrapper">
-            <a href="/" className="brand-logo">Logo</a>
+            <a href="/" className="brand-logo">DevOps</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
               <li><a href="/">HOME</a></li>
               <li><a href="https://www.facebook.com">FACEBOOK</a></li>
