cc0f8caf2566a496381c977be75cceb099564dd7
Vignesh Aalathur <85474399+vignesha04@users.noreply.github.com>
2024-04-06 16:46:07 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,23 +8,20 @@ export default class Footer extends Component {
                 <div className="container">
                   <div className="row">
                     <div className="col l6 s12">
-                      <h5 className="white-text">Footer Content</h5>
-                      <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                      <h5 className="white-text">Sample React Webapp by VICKY</h5>
+                      <p className="grey-text text-lighten-4"> ReactJs Application deployment by using Kubernetes.</p>
                     </div>
                     <div className="col l4 offset-l2 s12">
                       <h5 className="white-text">Links</h5>
                       <ul>
-                        <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                        <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                        <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                        <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                        <li><a className="grey-text text-lighten-3" href="#!">click here</a></li>
                       </ul>
                     </div>
                   </div>
                 </div>
                 <div className="footer-copyright">
                   <div className="container">
-                  © 2014 Copyright Text
+                  © 2024 Copyright Text
                   <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
                   </div>
                 </div>
